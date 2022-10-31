46cdcdb20f2d3ce8b72ae868e56eb5115a055095
Olivia Sarkisian <115608329+sarkisianolivia@users.noreply.github.com>
2022-10-31 00:32:24 -0500

    Add files via upload

---
+++ BarkingDog.java
@@ -0,0 +1,17 @@
+public class BarkingDog {
+
+    public static boolean shouldWakeUp (boolean barking , int hourOfDay ){
+
+        if (barking && ((hourOfDay >= 0) && (hourOfDay < 8)) || ((hourOfDay > 22) && (hourOfDay <= 23))) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+
+    }
+
+
+
+}
